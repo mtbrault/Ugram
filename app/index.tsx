b0ecb52a37025cb203c1799a08542dcb652c37ff
Matthieu BRAULT <matthieu.brault@epitech.eu>
2020-01-21 22:17:32 -0500

    create-react-app

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { App } from "./src/App";
-
-ReactDOM.render(<App  compiler="TypeScript" framework="React" />, document.getElementById('root'));
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,26 @@
-import * as React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export interface AppProps { compiler: string; framework: string; }
-
-export class App extends React.Component<AppProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+    </div>
+  );
 }
+
+export default App;
