43e8236adc98a367ad4536e84975247cbf15282e
Mehdi Makhloufi <mehdi.makhloufi@epitech.eu>
2020-01-22 17:04:59 -0500

    Update App.jsx

--- app/src/Components/App.tsx
+++ app/src/Components/App.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
-export interface AppProps { firstName?: string; }
-
-const App = ({ firstName }) => (
+export const App = ({ firstName }) => (
 	<h1>Hello World {firstName ? firstName : "Mehdi"}</h1>
 );
-
-export default App;
