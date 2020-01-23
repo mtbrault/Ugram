886e9b910fa737e99a4575a933e0e944666a13b6
Matthieu BRAULT <matthieu.brault@epitech.eu>
2020-01-22 19:36:04 -0500

    patching tsconfig.json

--- app/src/Components/App.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import "./../Style/_Styles.scss";
-
-type AppProps = { firstName?: string; }
-
-export const App = ({ firstName } : AppProps) => (
-	<h1>Hello World {firstName ? firstName : "Mehdi"}</h1>
-);
