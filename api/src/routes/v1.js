309ecd40559f8a5c63261c597957ee9e84df848b
Anachros <36197406+Anachros@users.noreply.github.com>
2020-01-26 20:18:05 -0500

    created basic server, routes, app and db files

---
+++ api/src/routes/v1.js
@@ -0,0 +1,43 @@
+const express = require('express');
+// const passport = require('passport');
+
+const router = express.Router();
+// const auth = require('../controllers/authController');
+// const user = require('../controllers/userController');
+
+
+//require('../middlewares/passport')(passport);
+
+// ** Banner **
+router.get('/', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "Welcome to ugram API",
+        data: { version: "v0.0.1" }
+    })
+});
+
+// ** Auth **
+// router.post('/auth/google', auth.google);
+// router.post('/auth/twitter', auth.twitter);
+// router.post('/auth/reddit', auth.reddit);
+// router.post('/auth/microsoft', auth.microsoft);
+
+// ** Auth and User **
+// router.post('/auth/login', user.login);
+// router.post('/auth/register', user.register);                                         //C
+
+// ** User **
+// router.get('/user', passport.authenticate('jwt', {session:false}), user.get);         //R
+//router.put('/user', passport.authenticate('jwt', {session:false}), user.update);    //U
+// router.delete('/user', passport.authenticate('jwt', {session:false}), user.delete);   //D
+
+// ** Services **
+// router.post('/user/google', passport.authenticate('jwt', {session:false}), user.google);       //C
+// router.post('/user/twitter', passport.authenticate('jwt', {session:false}), user.twitter);     //C
+// router.post('/user/reddit', passport.authenticate('jwt', {session:false}), user.reddit);       //C
+// router.post('/user/microsoft', passport.authenticate('jwt', {session:false}), user.microsoft); //C
+// router.get('/user/services', passport.authenticate('jwt', {session:false}), user.services)     //R
+
+
+module.exports = router;
