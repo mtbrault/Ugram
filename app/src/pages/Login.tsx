0957b4d6611b0adb040c6f5421cb8dcff252df83
Mehdi Makhloufi <mehdi.makhloufi@epitech.eu>
2020-02-01 17:51:44 -0500

    Update UI of Login and Register Page

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 
 import {
   Card, Row, Col, Input, Button,
@@ -8,44 +8,60 @@ import {
 interface LoginInputProps {
   id: string;
   title: string;
-  name: string;
   type: string;
   suffix?: ReactNode;
 }
 
 const LoginInput = ({
-  title, id, name, type, suffix,
+  title, id, type, suffix,
 } : LoginInputProps) => (
-  <Row type="flex" justify="center">
+  <Row type="flex" justify="center" className="input-container">
     <Col span={20}>
       <b>{title}</b>
-      <Input id={id} name={name} type={type} suffix={suffix} />
+      <Input id={id} type={type} suffix={suffix} />
     </Col>
   </Row>
 );
 
-const Login = () => (
-  <Row type="flex" justify="center">
-    <Col span={10}>
-      <Card bordered>
-        <LoginInput id="username" name="username" title="Nom d'utilisateur" type="text" />
-        <LoginInput
-          id="password"
-          name="password"
-          title="Mot de passe"
-          type="password"
-          suffix={<Button type="link" icon="eye" />}
-        />
-        <Row type="flex" justify="center">
-          <Col span={20}>
-            <Button type="primary" onClick={() => console.log('Connexion')}>
-              Connexion
-            </Button>
-          </Col>
-        </Row>
-      </Card>
-    </Col>
-  </Row>
-);
+const Login = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Row type="flex" justify="center">
+        <Col lg={7} md={10} sm={16} xs={24}>
+          <Card bordered>
+            <h1 className="text-center">Ugram</h1>
+            <LoginInput id="username" title="Nom d'utilisateur" type="text" />
+            <LoginInput
+              id="password"
+              title="Mot de passe"
+              type={show ? 'text' : 'password'}
+              suffix={<Button type="ghost" icon={show ? 'eye-invisible' : 'eye'} onClick={() => setShow(!show)} />}
+            />
+            <Row type="flex" justify="center">
+              <Col span={20} className="btn-center">
+                <Button type="primary" onClick={() => console.log('Connexion')}>
+                  Connexion
+                </Button>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center" className="container">
+        <Col lg={7} md={10} sm={16} xs={24} className="text-center">
+          <Card bordered>
+            <Row type="flex" align="middle" justify="start">
+              <Col span={12}>Vous n’avez pas de compte  ?</Col>
+              <Col span={12}>
+                <Button type="link" href="/register">Inscrivez-vous</Button>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Login;
--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 
 import {
   Card, Row, Col, Input, Button,
@@ -15,7 +15,7 @@ interface RegisterInputProps {
 const RegisterInput = ({
   title, id, type, suffix,
 } : RegisterInputProps) => (
-  <Row type="flex" justify="center">
+  <Row type="flex" justify="center" className="input-container">
     <Col span={20}>
       <b>{title}</b>
       <Input id={id} type={type} suffix={suffix} />
@@ -23,28 +23,46 @@ const RegisterInput = ({
   </Row>
 );
 
-const Register = () => (
-  <Row type="flex" justify="center">
-    <Col span={10}>
-      <Card bordered>
-        <RegisterInput id="username" title="Nom d'utilisateur" type="text" />
-        <RegisterInput id="name" title="Nom complet" type="text" />
-        <RegisterInput
-          id="password"
-          title="Mot de passe"
-          type="password"
-          suffix={<Button type="link" icon="eye" />}
-        />
-        <Row type="flex" justify="center">
-          <Col span={20}>
-            <Button type="primary" onClick={() => console.log('Connexion')}>
-              S'inscrire
-            </Button>
-          </Col>
-        </Row>
-      </Card>
-    </Col>
-  </Row>
-);
+const Register = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Row type="flex" justify="center">
+        <Col lg={7} md={10} sm={16} xs={24}>
+          <Card bordered>
+            <h1 className="text-center">Ugram</h1>
+            <RegisterInput id="username" title="Nom d'utilisateur" type="text" />
+            <RegisterInput id="name" title="Nom complet" type="text" />
+            <RegisterInput
+              id="password"
+              title="Mot de passe"
+              type={show ? 'text' : 'password'}
+              suffix={<Button type="ghost" icon={show ? 'eye-invisible' : 'eye'} onClick={() => setShow(!show)} />}
+            />
+            <Row type="flex" justify="center">
+              <Col span={20} className="btn-center">
+                <Button type="primary" onClick={() => console.log('S\'inscrire')}>
+                  S'inscrire
+                </Button>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center" className="container">
+        <Col lg={7} md={10} sm={16} xs={24} className="text-center">
+          <Card bordered>
+            <Row type="flex" align="middle" justify="start">
+              <Col span={12}>Vous avez un compte ?</Col>
+              <Col span={12}>
+                <Button type="link" href="/login">Connectez-vous</Button>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Register;
