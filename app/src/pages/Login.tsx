111a57f4dac8bee9a419ce6910afe99243e45b36
Mehdi Makhloufi <mehdi.makhloufi@epitech.eu>
2020-02-01 02:14:20 -0500

    Add style, ant packages, register/login v1 ui, update gitignore

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+
+import {
+  Card, Row, Col, Input, Button,
+} from 'antd/es';
+
+
+interface LoginInputProps {
+  id: string;
+  title: string;
+  name: string;
+  type: string;
+  suffix?: ReactNode;
+}
+
+const LoginInput = ({
+  title, id, name, type, suffix,
+} : LoginInputProps) => (
+  <Row type="flex" justify="center">
+    <Col span={20}>
+      <b>{title}</b>
+      <Input id={id} name={name} type={type} suffix={suffix} />
+    </Col>
+  </Row>
+);
 
 const Login = () => (
-  <p>Login</p>
+  <Row type="flex" justify="center">
+    <Col span={10}>
+      <Card bordered>
+        <LoginInput id="username" name="username" title="Nom d'utilisateur" type="text" />
+        <LoginInput
+          id="password"
+          name="password"
+          title="Mot de passe"
+          type="password"
+          suffix={<Button type="link" icon="eye" />}
+        />
+        <Row type="flex" justify="center">
+          <Col span={20}>
+            <Button type="primary" onClick={() => console.log('Connexion')}>
+              Connexion
+            </Button>
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+  </Row>
 );
 
 export default Login;
--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+
+import {
+  Card, Row, Col, Input, Button,
+} from 'antd/es';
+
+
+interface RegisterInputProps {
+  title: string;
+  id: string;
+  type: string;
+  suffix?: ReactNode;
+}
+
+const RegisterInput = ({
+  title, id, type, suffix,
+} : RegisterInputProps) => (
+  <Row type="flex" justify="center">
+    <Col span={20}>
+      <b>{title}</b>
+      <Input id={id} type={type} suffix={suffix} />
+    </Col>
+  </Row>
+);
 
 const Register = () => (
-  <p>Register</p>
+  <Row type="flex" justify="center">
+    <Col span={10}>
+      <Card bordered>
+        <RegisterInput id="username" title="Nom d'utilisateur" type="text" />
+        <RegisterInput id="name" title="Nom complet" type="text" />
+        <RegisterInput
+          id="password"
+          title="Mot de passe"
+          type="password"
+          suffix={<Button type="link" icon="eye" />}
+        />
+        <Row type="flex" justify="center">
+          <Col span={20}>
+            <Button type="primary" onClick={() => console.log('Connexion')}>
+              S'inscrire
+            </Button>
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+  </Row>
 );
 
 export default Register;
