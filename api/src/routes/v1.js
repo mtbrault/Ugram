b6df2eac27abecb018280874907c90eec263eb73
Anachros <36197406+Anachros@users.noreply.github.com>
2020-02-10 13:48:58 -0500

    add middleware usage

--- api/src/routes/v1.js
+++ api/src/routes/v1.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../controllers/authController');
 const user = require('../controllers/userController');
 
+const { isAdminOrLoggedUser } = require("../middlewares/validation");
 
 require('../middlewares/passport')(passport);
 
@@ -33,7 +34,7 @@ router.get('/user', passport.authenticate('jwt', {session:false}), user.get);   
 router.get('/user/:id', passport.authenticate('jwt', {session:false}), user.getById);         //R
 //router.put('/user', passport.authenticate('jwt', {session:false}), user.update);    //U
 router.delete('/user', passport.authenticate('jwt', {session:false}), user.remove);   //D
-router.delete('/user/:id', passport.authenticate('jwt', {session:false}), user.removeById);   //D
+router.delete('/user/:id', passport.authenticate('jwt', {session:false}), isAdminOrLoggedUser, user.removeById);   //D
 
 // ** Services **
 // router.post('/user/google', passport.authenticate('jwt', {session:false}), user.google);       //C
