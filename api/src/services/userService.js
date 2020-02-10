c4d501959b5fd512138ee68c22c5960e93f972d1
Anachros <36197406+Anachros@users.noreply.github.com>
2020-02-10 13:53:33 -0500

    hints when register fail, login with username, email or phonenumber

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,31 +1,52 @@
 const { User } = require("../db");
 
 const authenticate = async ({ username, password }) => {
-    if (!username || !password)
-        throw new Error("missing username or password");
-    let user = await User.findOne({ username: username.toLowerCase() });
-    if (!user)
-        throw new Error("Unknown user");
-    user = await user.comparePassword(password);
-    return user;
+	if (!username || !password)
+		throw new Error("missing username or password field");
+	let key = "username";
+	if (username.includes("@")){
+		key = "email";
+	} else if ("+0123456789".includes(username[0])) {
+		key = "phoneNumber";
+	}
+	let user = await User.findOne({ [key]: username.toLowerCase() });
+	if (!user)
+		throw new Error("Bad Credentials");
+	user = await user.comparePassword(password);
+	return user;
 };
 
-const create = async ({ username, password, firstname, lastname, email, profile_pic_url }) => {
-    if(!username || !password)
-        throw new Error("missing username or password");
-    let user = await User.findOne({ username: username.toLowerCase() });
-    if(user)
-        throw new Error("username already taken");
+const create = async ({
+	username, password, firstname, lastname,
+	email, profilePic, phoneNumber,
+	}) => {
+	for (let [key, value] of Object.entries({ username, password, email, phoneNumber}))
+		if(!value) throw new Error(`${key} field is required`);
+
+	let user = await User.findOne({ $or: [
+		{ username: username.toLowerCase() },
+		{ email: email.toLowerCase() },
+		{ phoneNumber }
+	]});
+	if(user) {
+		let reason = "username";
+		if(email === user.email) {
+			reason = "email address";
+		} else if(phoneNumber === user.phoneNumber) {
+			reason = "phone number";
+		}
+		throw new Error(`${reason} already taken`);
+	}
 	const data = {
-		username, password, displayname: username
+		username, password, displayname: username,
+		email, phoneNumber
 	};
-	if(firstname) data.firstname = firstname;
-	if(lastname) data.lastname = lastname;
-	if(email) data.email = email;
-	if(profile_pic_url) data.profile_pic_url = profile_pic_url;
-    user = new User(data);
-    user = await user.save();
-    return user;
+	for (let [key, value] of Object.entries({ firstname, lastname, profilePic }))
+		if(!!value) data[key] = value;
+
+	user = new User(data);
+	user = await user.save();
+	return user;
 }
 
 const getById = async id => {
@@ -36,20 +57,20 @@ const getById = async id => {
 }
 
 // module.exports.update = async (user, data) => {
-//     if(data._id)
-//         delete data._id;
+//	 if(data._id)
+//		 delete data._id;
 //
 //
 // }
 
 const remove = async (user) => {
-    const res = await User.findByIdAndDelete(user._id);
-    return res;
+	const res = await User.findByIdAndDelete(user._id);
+	return res;
 };
 
 const removeById = async (id) => {
-    const res = await User.findByIdAndDelete(id);
-    return res;
+	const res = await User.findByIdAndDelete(id);
+	return res;
 };
 
 module.exports = {
