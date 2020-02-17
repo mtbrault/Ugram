6a60e512c85533f374db0420ea62f54f663297e1
Anachros <36197406+Anachros@users.noreply.github.com>
2020-02-17 17:31:06 -0500

    modified comment model and post model

--- api/src/models/commentModel.js
+++ api/src/models/commentModel.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose');
+const votingPlugin = require('./plugins/votingPlugin');
 
 const CommentSchema = mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true
-    },
-    postId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Post',
-        // required: true
-    },
-    content: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    mentions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    reactions: [{
-         type: mongoose.Schema.Types.ObjectId,
-         ref: 'User',
-         liked: {
-             type: Boolean,
-             // default: false
-         }
-    }]
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	target: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Post',
+		required: true
+	},
+	content: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	hashtags: {
+		type: [{type: String, trim: true}],
+		required: false
+	},
+	mentions: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+		required: false
+	}
 }, { timestamps: true });
+
+CommentSchema.plugin(votingPlugin);
+
+CommentSchema.index({target: 1, author: 1});
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- api/src/models/postModel.js
+++ api/src/models/postModel.js
@@ -1,37 +1,32 @@
 const mongoose = require('mongoose');
+const votingPlugin = require('./plugins/votingPlugin');
 
 const PostSchema = mongoose.Schema({
-    userId : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true
-    },
-    imageUrl : {
-        type: String,
-        trim: true,
-        required: true
-    },
-    hashtags: [{
-        type: String,
-        trim: true,
-        // TODO: regexp
-    }],
-    mentions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    reactions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        liked: {
-            type: Boolean,
-            // default: false
-        }
-    }],
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }]
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	imageUrl: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	description: {
+		type: String,
+		trim: true,
+		required: false,
+	},
+	hashtags: {
+		type: [{type: String, trim: true}],
+		required: false
+	},
+	mentions: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+		required: false
+	}
 }, { timestamps: true });
+
+PostSchema.plugin(votingPlugin);
 
 module.exports = mongoose.model('Post', PostSchema);
