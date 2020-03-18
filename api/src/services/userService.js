06347d5a3d40c6842176a1826704cae7cd1a39b0
Olivier <olivier.garcia-telouk@epitech.eu>
2020-03-18 16:57:10 -0400

    FIX delete user now delete his post

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,4 +1,4 @@
-const { User } = require("../db");
+const { User, Post } = require("../db");
 const { terr } = require("../middlewares/utils");
 
 const authenticate = async ({ username, password }) => {
@@ -120,6 +120,7 @@ const update = async (user, {
 }
 
 const remove = async (user) => {
+	await Post.deleteMany({"author.id": user._id});
 	return User.findByIdAndDelete(user._id);
 };
 
