4f980ab9b53f9f21da4c08709917b992c0f55ca1
Anachros <36197406+Anachros@users.noreply.github.com>
2020-04-13 22:19:08 -0400

    lint routes

--- api/src/routes/commentRouter.js
+++ api/src/routes/commentRouter.js
@@ -1,21 +1,22 @@
-const comment = require('../controllers/commentController');
+const comment = require("../controllers/commentController");
 const {
-	isValidUserId, isAdminOrLoggedUser,
-	isValidPostId, isAdminOrPostAuthor,
-	isValidCommentId, isAdminOrCommentAuthor
+	isValidUserId,
+	isValidPostId,
+	isValidCommentId,
+	isAdminOrCommentAuthor,
 } = require("../middlewares/validation");
 
 const { extractPageParams } = require("../middlewares/query");
 
-const commentRouter = router => {
+const commentRouter = (router) => {
 	// ** Comment **
-	router.post('/post/:id/comment', isValidPostId("id"), comment.create); //C
-	router.get('/self/comment', extractPageParams, comment.getSelf); //R
-	router.get('/user/:id/comment', isValidUserId("id"), extractPageParams, comment.getByUser); //R
-	router.get('/post/:id/comment', isValidPostId("id"), extractPageParams, comment.getByPost); //R
-	router.get('/comment/:id', isValidCommentId("id"), comment.getById); //R
-	router.patch('/comment/:id', isValidCommentId("id"), isAdminOrCommentAuthor, comment.update); //U
-	router.delete('/comment/:id', isValidCommentId("id"), isAdminOrCommentAuthor, comment.remove); //D
+	router.post("/post/:id/comment", isValidPostId("id"), comment.create); // C
+	router.get("/self/comment", extractPageParams, comment.getSelf); // R
+	router.get("/user/:id/comment", isValidUserId("id"), extractPageParams, comment.getByUser); // R
+	router.get("/post/:id/comment", isValidPostId("id"), extractPageParams, comment.getByPost); // R
+	router.get("/comment/:id", isValidCommentId("id"), comment.getById); // R
+	router.patch("/comment/:id", isValidCommentId("id"), isAdminOrCommentAuthor, comment.update); // U
+	router.delete("/comment/:id", isValidCommentId("id"), isAdminOrCommentAuthor, comment.remove); // D
 	return router;
 };
 
--- api/src/routes/postRouter.js
+++ api/src/routes/postRouter.js
@@ -1,18 +1,23 @@
-const post = require('../controllers/postController');
-const { isValidUserId, isAdminOrLoggedUser, isValidPostId, isAdminOrPostAuthor } = require("../middlewares/validation");
-const { extractPageParams, extractPostParams, extractDateParams} = require("../middlewares/query");
+const post = require("../controllers/postController");
+const {
+	isValidUserId,
+	isAdminOrLoggedUser,
+	isValidPostId,
+	isAdminOrPostAuthor,
+} = require("../middlewares/validation");
+const { extractPageParams, extractPostParams, extractDateParams } = require("../middlewares/query");
 
-const postRouter = router => {
+const postRouter = (router) => {
 	// ** Post **
-	router.post('/post', post.upload); //C
-	router.post('/user/:id/post', isValidUserId("id"), isAdminOrLoggedUser, post.uploadForUser); //C
-	router.get('/post', extractPageParams, extractPostParams, post.getAll); //R
-	router.get('/self/post', extractPageParams, post.getSelf); //R
-	router.get('/user/:id/post', isValidUserId("id"), extractPageParams, post.getByUser); //R
-	router.get('/post/:id', isValidPostId("id"), post.getById); //R
-	router.get('/keyword', extractPageParams, extractDateParams, post.getKeyword);
-	router.patch('/post/:id', isValidPostId("id"), isAdminOrPostAuthor, post.update); //U
-	router.delete('/post/:id', isValidPostId("id"), isAdminOrPostAuthor, post.remove); //D
+	router.post("/post", post.upload); // C
+	router.post("/user/:id/post", isValidUserId("id"), isAdminOrLoggedUser, post.uploadForUser); // C
+	router.get("/post", extractPageParams, extractPostParams, post.getAll); // R
+	router.get("/self/post", extractPageParams, post.getSelf); // R
+	router.get("/user/:id/post", isValidUserId("id"), extractPageParams, post.getByUser); // R
+	router.get("/post/:id", isValidPostId("id"), post.getById); // R
+	router.get("/keyword", extractPageParams, extractDateParams, post.getKeyword);
+	router.patch("/post/:id", isValidPostId("id"), isAdminOrPostAuthor, post.update); // U
+	router.delete("/post/:id", isValidPostId("id"), isAdminOrPostAuthor, post.remove); // D
 	return router;
 };
 
