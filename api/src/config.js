f551563b3307b4d531c504d6d9afc8c23cd2d648
Anachros <36197406+Anachros@users.noreply.github.com>
2020-04-13 22:20:58 -0400

    lint base files

--- api/src/config.js
+++ api/src/config.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
 	app: {},
 	api: {},
 	db: {},
 	jwt: {},
-	google: {}
+	google: {},
 };
 
-config.app.env             = process.env.APP || 'dev';
-config.app.port            = process.env.PORT || '8081';
-config.api.defaultPageSize = process.env.API_DEFAULT_PAGE_SIZE || '20';
-config.api.maxPageSize     = process.env.API_MAX_PAGE_SIZE || '100';
-config.db.dialect          = process.env.DB_DIALECT || 'mongodb';
-config.db.host             = process.env.DB_HOST || '127.0.0.1';
-config.db.port             = process.env.DB_PORT || '27017';
-config.db.name             = process.env.DB_NAME || 'ugram';
-config.db.user             = process.env.DB_USER || '';
-config.db.password         = process.env.DB_PASSWORD || '';
-config.db.params           = process.env.DB_PARAMS || '';
-config.jwt.secret          = process.env.JWT_SECRET || 'kkIX7SDOnb67MmTgU5cvko4XaN2wqzQczii4GBBFYPW8xotJhTh4J2sGys6QHr76';
-config.jwt.expiration      = process.env.JWT_EXPIRATION || '10000';
-config.google.clientId     = process.env.GOOGLE_CLIENT_ID;
+config.app.env = process.env.APP || "development";
+config.app.port = process.env.PORT || "8081";
+config.api.defaultPageSize = process.env.API_DEFAULT_PAGE_SIZE || "20";
+config.api.maxPageSize = process.env.API_MAX_PAGE_SIZE || "100";
+config.db.dialect = process.env.DB_DIALECT || "mongodb";
+config.db.host = process.env.DB_HOST || "127.0.0.1";
+config.db.port = process.env.DB_PORT || "27017";
+config.db.name = process.env.DB_NAME || "ugram";
+config.db.user = process.env.DB_USER || "";
+config.db.password = process.env.DB_PASSWORD || "";
+config.db.params = process.env.DB_PARAMS || "";
+config.jwt.secret =
+	process.env.JWT_SECRET || "kkIX7SDOnb67MmTgU5cvko4XaN2wqzQczii4GBBFYPW8xotJhTh4J2sGys6QHr76";
+config.jwt.expiration = process.env.JWT_EXPIRATION || "10000";
+config.google.clientId = process.env.GOOGLE_CLIENT_ID;
 
-if (!config.google.clientId)
-	throw new Error("google configuration missing");
+if (!config.google.clientId) throw new Error("google configuration missing");
 
 module.exports = config;
--- api/src/db.js
+++ api/src/db.js
@@ -1,36 +1,36 @@
+/* eslint-disable no-console */
 const mongoose = require("mongoose");
 const pe = require("parse-error");
-
-const { db } = require('./config');
-const userpass = (db.user && db.password) ? `${db.user}:${db.password}@`: '';
-const dbparams = !!db.params ? `?${db.params}` : '';
-const dbport = (db.dialect === "mongodb+srv") ? '' : `:${db.port}`;
-const db_url = `${db.dialect}://${userpass}${db.host}${dbport}/${db.name}${dbparams}`;
+const { db } = require("./config");
+const userpass = db.user && db.password ? `${db.user}:${db.password}@` : "";
+const dbparams = db.params ? `?${db.params}` : "";
+const dbport = db.dialect === "mongodb+srv" ? "" : `:${db.port}`;
+const dbUrl = `${db.dialect}://${userpass}${db.host}${dbport}/${db.name}${dbparams}`;
 
 const settings = {
 	useUnifiedTopology: true,
 	useNewUrlParser: true,
 	useCreateIndex: true,
-	useFindAndModify: false
+	useFindAndModify: false,
 };
 
-for (let [key, value] of Object.entries(settings))
-	mongoose.set(key, value);
+for (const [key, value] of Object.entries(settings)) mongoose.set(key, value);
 
-mongoose.connect(db_url)
-.then(() => {
-	console.log('Connected to mongoDB');
-}, e => {
-	console.error('Error while DB connecting: ', pe(e));
-	throw e;
-});
-
+mongoose.connect(dbUrl).then(
+	() => {
+		console.log("Connected to mongoDB");
+	},
+	(e) => {
+		console.error("Error while DB connecting: ", pe(e));
+		throw e;
+	},
+);
 
 module.exports = {
 	User: require("./models/userModel"),
 	Post: require("./models/postModel"),
 	Comment: require("./models/commentModel"),
-	Notification : require("./models/notificationModel"),
+	Notification: require("./models/notificationModel"),
 	Keyword: require("./models/keywordModel"),
-	connection: mongoose.connection
+	connection: mongoose.connection,
 };
