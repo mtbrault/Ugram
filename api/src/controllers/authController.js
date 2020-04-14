4066244fcb989ae0127245dbf6e44d8ea71c0d61
Anachros <36197406+Anachros@users.noreply.github.com>
2020-04-13 22:20:07 -0400

    lint controllers

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,31 +1,27 @@
-const userService = require('../services/userService');
-const googleService = require('../services/googleService');
-const { to, rerr } = require('../middlewares/utils');
+const userService = require("../services/userService");
+const googleService = require("../services/googleService");
+const { to, rerr } = require("../middlewares/utils");
 
 const login = async (req, res, next) => {
-	let [err, user] = await to(userService.authenticate(req.body));
-	if (err)
-		return rerr(next, err, 401);
+	const [err, user] = await to(userService.authenticate(req.body));
+	if (err) return rerr(next, err, 401);
 	return res.status(200).json({ token: user.getJWT(), ...user.toWeb() });
-}
+};
 
 const register = async (req, res, next) => {
-	let [err, user] = await to(userService.create(req.body));
-	if (err)
-		return rerr(next, err);
+	const [err, user] = await to(userService.create(req.body));
+	if (err) return rerr(next, err);
 	return res.status(201).json({ token: user.getJWT(), ...user.toWeb() });
-}
+};
 
-const tokeninfo = async (req, res, next) => {
+const tokeninfo = async (req, res, _next) => {
 	return res.status(204).send();
 };
 
 const google = async (req, res, next) => {
 	const [err, ret] = await to(googleService.authenticate(req.body));
-	if (err) {
-		console.log(err);
-		return rerr(next, err);
-	}
+	if (err) return rerr(next, err);
+
 	const { user, created } = ret;
 	return res.status(created ? 201 : 200).json({ token: user.getJWT(), ...user.toWeb() });
 };
@@ -34,5 +30,5 @@ module.exports = {
 	login,
 	register,
 	tokeninfo,
-	google
+	google,
 };
--- api/src/controllers/notificationController.js
+++ api/src/controllers/notificationController.js
@@ -1,71 +1,67 @@
-const notificationService = require('../services/notificationService');
-const { to, rerr } = require('../middlewares/utils');
+const notificationService = require("../services/notificationService");
+const { to, rerr } = require("../middlewares/utils");
 
 // NOTIFICATIONS
 
 // Authenticated calls
 
-//use only after extractPageParams
-const  getSelf = async (req, res, next) => {
-    const [err, ret] = await to(notificationService.getByUser(req.user, req.skip, req.limit));
-    if (err)
-        return rerr(next, err);
-    const {last, notifications} = ret;
-    const chunk = {
-        page: req.page,
-        limit: req.limit,
-        count: notifications.length,
-        notifications
-    };
-    if (!last)
-        chunk.next = `/self/notification?page=${req.page + 1}&limit=${req.limit}`;
-    return res.status(200).json(chunk);
+// Use only after extractPageParams
+const getSelf = async (req, res, next) => {
+	const [err, ret] = await to(notificationService.getByUser(req.user, req.skip, req.limit));
+	if (err) return rerr(next, err);
+	const { last, notifications } = ret;
+	const chunk = {
+		page: req.page,
+		limit: req.limit,
+		count: notifications.length,
+		notifications,
+	};
+	if (!last) chunk.next = `/self/notification?page=${req.page + 1}&limit=${req.limit}`;
+	return res.status(200).json(chunk);
 };
 
-// use only after isValidUserId and extractPageParams
+// Use only after isValidUserId and extractPageParams
 const getByUser = async (req, res, next) => {
-    const [err, ret] = await to(notificationService.getByUser(req.refUser, req.skip, req.limit));
-    if (err)
-        return rerr(next, err);
-    const {last, notifications} = ret;
-    const chunk = {
-        page: req.page,
-        limit: req.limit,
-        count: notifications.length,
-        notifications
-    };
-    if (!last)
-        chunk.next = `/user/${req.refUser._id}/notification?page=${req.page + 1}&limit=${req.limit}`;
-    return res.status(200).json(chunk);
+	const [err, ret] = await to(notificationService.getByUser(req.refUser, req.skip, req.limit));
+	if (err) return rerr(next, err);
+	const { last, notifications } = ret;
+	const chunk = {
+		page: req.page,
+		limit: req.limit,
+		count: notifications.length,
+		notifications,
+	};
+	if (!last)
+		chunk.next = `/user/${req.refUser._id}/notification?page=${req.page + 1}&limit=${
+			req.limit
+		}`;
+	return res.status(200).json(chunk);
 };
 
-// use only after isValidNotificationId
+// Use only after isValidNotificationId
 const markAsRead = async (req, res, next) => {
-    const [err, notification] = await to(notificationService.markAsRead(req.refNotification));
-    if (err)
-        return rerr(next, err);
-    return res.status(200).json(notification.toWeb());
+	const [err, notification] = await to(notificationService.markAsRead(req.refNotification));
+	if (err) return rerr(next, err);
+	return res.status(200).json(notification.toWeb());
 };
 
 const markAllAsRead = async (req, res, next) => {
-    const [err, notification] = await to(notificationService.markAllAsRead(req.user));
-    if (err)
-        return rerr(next, err);
-    return res.status(204).send();
+	const [err] = await to(notificationService.markAllAsRead(req.user));
+	if (err) return rerr(next, err);
+	return res.status(204).send();
 };
 
-// use only after isValidNotificationId
+// Use only after isValidNotificationId
 const remove = async (req, res, next) => {
-    const [err, notification] = await to(notificationService.remove(req.refNotification));
-    if (err)
-        return rerr(next, err, 500);
-    return res.status(204).send();
+	const [err] = await to(notificationService.remove(req.refNotification));
+	if (err) return rerr(next, err, 500);
+	return res.status(204).send();
 };
 
 module.exports = {
-    getSelf,
-    getByUser,
-    markAsRead,
-    markAllAsRead,
-    remove
+	getSelf,
+	getByUser,
+	markAsRead,
+	markAllAsRead,
+	remove,
 };
