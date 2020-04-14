fd2fbe2699bcf66557c14279f0e57a73516d7d82
Anachros <36197406+Anachros@users.noreply.github.com>
2020-04-13 22:19:41 -0400

    lint middlewares

--- api/src/middlewares/query.js
+++ api/src/middlewares/query.js
@@ -1,43 +1,50 @@
-const { api } = require('../config');
+const { api } = require("../config");
 
-let conv = parseInt(api.defaultPageSize, 10) || 20;
+let conv = parseInt(api.defaultPageSize, 10) || 20;
 const defaultPageSize = conv < 1 ? 20 : conv;
 
-conv = parseInt(api.maxPageSize, 10) || 100;
+conv = parseInt(api.maxPageSize, 10) || 100;
 const maxPageSize = conv < defaultPageSize ? defaultPageSize : conv;
 
 const wildcards = {
-	END: 'end',
-	FULL: 'full',
-	NONE: 'none'
+	END: "end",
+	FULL: "full",
+	NONE: "none",
 };
 
-function formatRegKeyValue (key, value, wildCardType) {
-	let obj = {};
+function formatRegKeyValue(key, value, wildCardType) {
+	const obj = {};
 	switch (wildCardType) {
-		case ('none'):
-			obj[key] = value;
-			break;
-		case ('full'):
+		case "full":
 			obj[key] = new RegExp(`.*${value}.*`, "i");
 			break;
-		case ('end'):
+		case "end":
 			obj[key] = new RegExp(`^${value}`, "i");
 			break;
+		default:
+			obj[key] = value;
 	}
+
 	return obj;
 }
 
 const extractQueryParams = (req, res, next, listParam) => {
 	req.requestParam = [];
-	var wildCardType = req.query.autocomplete === 'true' ? wildcards.END : wildcards.FULL;
+	const wildCardType = req.query.autocomplete === "true" ? wildcards.END : wildcards.FULL;
 	Object.keys(req.query).forEach(function (key) {
-		for (param of listParam) {
+		for (const param of listParam) {
 			if (key === param.name) {
-				req.requestParam.push(formatRegKeyValue(param.name, req.query[key],
-					param.isWild ? wildCardType : wildcards.NONE));
+				req.requestParam.push(
+					formatRegKeyValue(
+						param.name,
+						req.query[key],
+						param.isWild ? wildCardType : wildcards.NONE,
+					),
+				);
 			} else if (key === "content") {
-				req.requestParam.push(formatRegKeyValue(param.name, req.query.content, wildCardType));
+				req.requestParam.push(
+					formatRegKeyValue(param.name, req.query.content, wildCardType),
+				);
 			}
 		}
 	});
@@ -45,48 +52,50 @@ const extractQueryParams = (req, res, next, listParam) => {
 };
 
 const extractUserParams = (req, res, next) => {
-	extractQueryParams(req, res, next,
-		[{name: "displayname", isWild: true}, {name: "username", isWild: true},
-			{name: "firstname", isWild: true}, {name: "lastname", isWild: true},
-			{name: "email", isWild: true}]);
+	extractQueryParams(req, res, next, [
+		{ name: "displayname", isWild: true },
+		{ name: "username", isWild: true },
+		{ name: "firstname", isWild: true },
+		{ name: "lastname", isWild: true },
+		{ name: "email", isWild: true },
+	]);
 };
 
 const extractPostParams = (req, res, next) => {
-	extractQueryParams(req, res, next, [{name: "hashtags", isWild: false},
-		{name: "description", isWild: true}]);
+	extractQueryParams(req, res, next, [
+		{ name: "hashtags", isWild: false },
+		{ name: "description", isWild: true },
+	]);
 };
 
 const extractDateParams = (req, res, next) => {
-	var now = new Date();
+	const now = new Date();
 	if (req.query) {
 		switch (req.query.date) {
-			default :
-			case ('year'):
+			default:
+			case "year":
 				now.setFullYear(now.getFullYear() - 1);
 				break;
-			case ('month'):
-				if (now.getMonth() === 0)
-					now.setFullYear(now.getFullYear() - 1);
-				now.setMonth(now.getMonth() - 1)
+			case "month":
+				if (now.getMonth() === 0) now.setFullYear(now.getFullYear() - 1);
+				now.setMonth(now.getMonth() - 1);
 				break;
-			case ('day'):
-				if (now.getDate() === 1)
-					now.setMonth(now.getMonth() - 1);
+			case "day":
+				if (now.getDate() === 1) now.setMonth(now.getMonth() - 1);
 				now.setDate(now.getDate() - 1);
 				break;
 		}
 	}
+
 	req.dateLimit = now;
 	next();
 };
 
-const extractPageParams = (req,res,next) => {
+const extractPageParams = (req, res, next) => {
 	req.page = req.query.page ? parseInt(req.query.page, 10) : 0;
-	if(isNaN(req.page) || req.page < 0)
-		req.page = 0;
-	req.limit = req.query.limit ? parseInt(req.query.limit, 10) || 0 : defaultPageSize;
-	if (req.limit < 1 || req.limit > maxPageSize)
-		req.limit = maxPageSize;
+	if (isNaN(req.page) || req.page < 0) req.page = 0;
+	req.limit = req.query.limit ? parseInt(req.query.limit, 10) || 0 : defaultPageSize;
+	if (req.limit < 1 || req.limit > maxPageSize) req.limit = maxPageSize;
 	req.skip = req.query.page * req.query.limit;
 	next();
 };
--- api/src/middlewares/utils.js
+++ api/src/middlewares/utils.js
@@ -1,40 +1,31 @@
-const to = (p, base_err = null) => {
+const to = (p, baseErr = null) => {
 	return p
-		.then(data => [null, data])
-		.catch(err => {
-			if (base_err) Object.assign(err, base_err);
+		.then((data) => [null, data])
+		.catch((err) => {
+			if (baseErr) Object.assign(err, baseErr);
 			return [err, undefined];
 		});
 };
 
-const asyncForEach = async (array, callback) => {
-	for (let i = 0; i < array.length; i++) {
-		await callback(array[i], i, array);
-	}
-};
-
-const rerr = (next, err, code=500) => {
-	if(typeof err == 'string')
-		err = new Error(err);
-	if(!err.status)
-		err.status = code;
+const rerr = (next, err, code = 500) => {
+	if (typeof err === "string") err = new Error(err);
+	if (!err.status) err.status = code;
 	next(err);
 };
 
-const terr = (message, code=500) => {
+const terr = (message, code = 500) => {
 	const err = new Error(message);
 	err.status = code;
 	throw err;
 };
 
-const notEmpty = x => {
+const notEmpty = (x) => {
 	return Array.isArray(x) && x.length > 0;
 };
 
 module.exports = {
 	to,
-	asyncForEach,
 	rerr,
 	terr,
-	notEmpty
+	notEmpty,
 };
